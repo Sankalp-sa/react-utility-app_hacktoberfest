05496466c631e157c0ba50e862f8c55b80894bb7
Sankalp-sa <sankalp.p@ahduni.edu.in>
2023-10-01 00:20:26 +0530

    Added border to the add task

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import animation from './deleteButtonAnimation.js';
 
 function TaskList({ tasks, onDeleteTask }) {
+
   return (
-    <ul className="task-list">
+   <div className="container task-div"> 
+    <ul className="task-list p-4">
       {tasks.map((task) => (
         <li key={task._id}>
           {task.title}
-          <button onClick={() => onDeleteTask(task._id)}>Delete</button>
+          <button onClick={() => onDeleteTask(task._id)} className='btn btn-danger ms-5'>
+            Delete
+          </button>
         </li>
       ))}
     </ul>
+    </div>
   );
 }
 
---
+++ src/deleteButtonAnimation.js
@@ -0,0 +1,16 @@
+const animation = () =>{
+
+    document.querySelectorAll('.button').forEach(button => button.addEventListener('click', e => {
+        if(!button.classList.contains('delete')) {
+    
+            button.classList.add('delete');
+    
+            setTimeout(() => button.classList.remove('delete'), 2200);
+    
+        }
+        e.preventDefault();
+    }));
+    
+}
+
+export default animation;
