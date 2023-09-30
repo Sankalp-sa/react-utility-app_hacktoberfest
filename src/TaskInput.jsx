cc6e94c8d45da5c22e0c981b576528073d69e1cb
Sankalp-sa <sankalp.p@ahduni.edu.in>
2023-10-01 00:33:41 +0530

    Changed ui of input button

--- src/TaskInput.jsx
+++ src/TaskInput.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
 
 function TaskInput({ onAddTask }) {
   const [newTask, setNewTask] = useState('');
@@ -10,15 +11,22 @@ function TaskInput({ onAddTask }) {
   };
 
   return (
-    <div className="task-input">
-      <input
-        type="text"
-        placeholder="Add a new task"
-        value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}
-      />
-      <button onClick={handleAddTask}>Add</button>
-    </div>
+    <>
+      <div className="input-group my-4">
+        <input type="text" className="form-control" placeholder="Add your task" aria-label="Recipient's username" aria-describedby="button-addon2" value={newTask}
+          onChange={(e) => setNewTask(e.target.value)} />
+        <button className="btn btn-primary pb-2" type="button" id="button-addon2" onClick={handleAddTask}><AiOutlinePlusCircle /></button>
+      </div>
+      {/* <div className="task-input">
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add</button>
+      </div> */}
+    </>
   );
 }
 
--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -7,7 +7,7 @@ function TaskList({ tasks, onDeleteTask }) {
    <div className="container task-div"> 
     <ul className="task-list p-4">
       {tasks.map((task) => (
-        <li key={task._id}>
+        <li key={task._id} className='mb-3'>
           {task.title}
           <button onClick={() => onDeleteTask(task._id)} className='btn btn-danger ms-5'>
             Delete
